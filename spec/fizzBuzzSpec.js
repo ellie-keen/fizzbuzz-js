6fdef9c2dc3d9c1374cf7750707a37284691a2ed
Mark Channer <markchanner@gmail.com>
2018-05-15 14:18:20 +0100

    Adds methods to test for divisible by 3, 5, and 15

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,23 +1,34 @@
 describe('Javabuzz', function() {
-
   var javabuzz;
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
 
   describe('know when a number is', function() {
-
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBeTruthy();
     });
 
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBeTruthy();
+    });
+
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(15)).toBeTruthy();
+    });
   });
 
   describe('know when a number is NOT', function() {
-
-    it('divisible by 3', function(){
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(4)).not.toBe(true);
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(4)).not.toBeTruthy();
     });
 
-  });
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(6)).not.toBeTruthy();
+    });
 
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(16)).not.toBeTruthy();
+    });
+  });
 });
