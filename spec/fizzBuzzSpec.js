cb8d1b47b028c3d831de11f45e59722a6573305b
Ellie Keen <elliekeen95@gmail.com>
2018-05-15 10:18:57 +0100

    Fizzbuzz isDivisibleByThree method

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,23 @@
+describe('Javabuzz', function() {
+
+  var javabuzz;
+
+  describe('know when a number is', function() {
+
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+  });
+
+  describe('know when a number is NOT', function() {
+
+    it('divisible by 3', function(){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(4)).not.toBe(true);
+    });
+
+  });
+
+});
